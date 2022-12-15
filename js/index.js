8e53050bf6dc27c4ffb7aef143d6a3eb0aeb149e
David <david.owuor@student.moringaschool.com>
2022-12-16 02:34:47 +0300

    second commit

--- js/index.js
+++ js/index.js
@@ -23,3 +23,50 @@ function displayImage(data){
   document.querySelector('#name').innerHTML = data.name
   document.querySelector('#description').innerHTML = data.description
 }
+
+
+
+
+
+
+fetch('http://localhost:3000/meals')
+.then(res=>{
+  return res.json()
+}).then(data=>{
+  //console.log(data);
+
+ loopImage(data)
+
+})
+
+
+let time = 1000
+
+function loopImage(items){
+  for(let item of items){
+    imageArray = item.img
+   // console.log(imageArray);
+    let arrayNew =[]
+    arrayNew.push(imageArray)
+    console.log(arrayNew);
+    let i = 0
+    
+   document.querySelector('.slideshow').src = arrayNew[i]
+    if(i<arrayNew.length -1){
+        i++
+    }else{
+      i =0 
+    }
+   
+    
+  }
+ setTimeout('loopImage()',time)
+}
+
+
+
+
+function displayImages(images){
+
+}
+
