e84d89d6ed97d5babb67f49961365f558a6db598
David <david.owuor@student.moringaschool.com>
2022-12-16 06:22:26 +0300

    third commit

--- js/index.js
+++ js/index.js
@@ -29,44 +29,78 @@ function displayImage(data){
 
 
 
-fetch('http://localhost:3000/meals')
-.then(res=>{
-  return res.json()
-}).then(data=>{
-  //console.log(data);
+// fetch('http://localhost:3000/meals')
+// .then(res=>{
+//   return res.json()
+// }).then(data=>{
+//   //console.log(data);
 
- loopImage(data)
+//  loopImage(data)
 
-})
+// })
 
 
-let time = 1000
+// let time = 1000
 
-function loopImage(items){
-  for(let item of items){
-    imageArray = item.img
-   // console.log(imageArray);
-    let arrayNew =[]
-    arrayNew.push(imageArray)
-    console.log(arrayNew);
-    let i = 0
+// function loopImage(items){
+//   for(let item of items){
+//     imageArray = item.img
+//    // console.log(imageArray);
+//     let arrayNew =[]
+//     arrayNew.push(imageArray)
+//     console.log(arrayNew);
+//     let i = 0
     
-   document.querySelector('.slideshow').src = arrayNew[i]
-    if(i<arrayNew.length -1){
-        i++
-    }else{
-      i =0 
-    }
+//    document.querySelector('.slideshow').src = arrayNew[i]
+//     if(i<arrayNew.length -1){
+//         i++
+//     }else{
+//       i =0 
+//     }
    
     
+//   }
+//  setTimeout('loopImage()',time)
+// }
+
+
+
+
+// function displayImages(images){
+
+// }
+
+
+
+let forms = document.querySelector('#form')
+forms.addEventListener('submit', (e)=>{
+  e.preventDefault()
+
+  let name = document.querySelector('#names').value
+  let email = document.querySelector('#emails').value
+  let des = document.querySelector('#desig').value
+
+  let obj ={
+    name : name,
+    email: email,
+    des : des
   }
- setTimeout('loopImage()',time)
-}
 
 
 
+  const config = {
+    method: 'POST',
+    headers: {
+      "Content-Type": 'application/json',
+      Accept: "application/json",
+    },
+    body: JSON.stringify(obj)
+  }
 
-function displayImages(images){
-
-}
-
+  fetch('http://localhost:3000/signups',config).then(res=>{
+    return res.json()
+  }).then(data=>{
+    console.log(data);
+  }).catch(error=>console.log(error))
+  
+})
