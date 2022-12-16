4d00358eeb199e20016e21a7e4c730182e67da33
David <david.owuor@student.moringaschool.com>
2022-12-16 07:09:58 +0300

    morning commit

--- js/index.js
+++ js/index.js
@@ -29,46 +29,9 @@ function displayImage(data){
 
 
 
-// fetch('http://localhost:3000/meals')
-// .then(res=>{
-//   return res.json()
-// }).then(data=>{
-//   //console.log(data);
-
-//  loopImage(data)
-
-// })
-
-
-// let time = 1000
-
-// function loopImage(items){
-//   for(let item of items){
-//     imageArray = item.img
-//    // console.log(imageArray);
-//     let arrayNew =[]
-//     arrayNew.push(imageArray)
-//     console.log(arrayNew);
-//     let i = 0
-    
-//    document.querySelector('.slideshow').src = arrayNew[i]
-//     if(i<arrayNew.length -1){
-//         i++
-//     }else{
-//       i =0 
-//     }
-   
-    
-//   }
-//  setTimeout('loopImage()',time)
-// }
 
 
 
-
-// function displayImages(images){
-
-// }
 
 
 
